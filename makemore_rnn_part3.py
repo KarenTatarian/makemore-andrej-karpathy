c94cb90edff102297074e8485e076bc89077b324
KarenTatarian <tatariankaren@gmail.com>
2026-09-11 11:05:27 -0400

    building torch like library and neural net and tools for measuring performance

--- makemore_rnn_part3.py
+++ makemore_rnn_part3.py
@@ -46,9 +46,11 @@ n_hidden = 200 #number of neurons in the hidden layer of MLP
 g = torch.Generator().manual_seed(2147483647) # for reproducibility
 C  = torch.randn((vocab_size, n_embd),            generator=g)
 W1 = torch.randn((n_embd * block_size, n_hidden), generator=g) * (5/3)/(n_embd*block_size)**0.5 #0.2
-#b1 = torch.randn(n_hidden,                        generator=g) * 0.01 #setting it very small to get a little bit of entropy (variation and diversity in initialization to help optimization)
+#b1 = torch.randn(n_hidden,                        generator=g) 
+#* 0.01 #setting it very small to get a little bit of entropy (variation and diversity in initialization to help optimization)
 W2 = torch.randn((n_hidden, vocab_size),          generator=g) * 0.01
-b2 = torch.randn(vocab_size,                      generator=g) * 0 #equivalent to torch.zeros(vocab_size), zero initialization does not mean it is frozen, b2 still gets trained
+b2 = torch.randn(vocab_size,                      generator=g) * 0 
+#equivalent to torch.zeros(vocab_size), zero initialization does not mean it is frozen, b2 still gets trained
  
 bngain = torch.ones((1, n_hidden))
 bnbias = torch.zeros((1, n_hidden))
@@ -62,7 +64,7 @@ print(sum(p.nelement() for p in parameters)) # number of parameters in total
 for p in parameters:
   p.requires_grad = True
 
-max_steps = 200000
+max_steps = 2000 #200000
 batch_size = 32
 lossi = []
 
@@ -97,7 +99,7 @@ for i in range(max_steps):
         p.grad = None
     loss.backward()
 
-    lr = 0.1 if i < 10000 else 0.01
+    lr = 0.1 if i < 1000 else 0.01
     #update parameters
     for p in parameters:
         p.data += -lr*p.grad 
@@ -165,3 +167,199 @@ for _ in range(20):
             break
     print(''.join(itos[i] for i in out))
 
+#---BRINGING IT TOGETHER-----
+
+class Linear:
+
+    def __init__(self, fan_in, fan_out, bias = True):
+        self.weight = torch.randn((fan_in, fan_out), generator = g)/fan_in**0.5 
+        #fan_in and fan_out here giving the dimensions fan_in x fan_out with mean around 0 and std 1 but then with 1/sqrt(fan_in) distribution is scaled down 
+        #the (1/fan_in**0.5) factor can be removed once normalization layers are added everywhere 
+        
+        self.bias = torch.zeros(fan_out) if bias else None 
+
+    def __call__(self, x):
+        self.out = x @ self.weight
+        if self.bias is not None:
+            self.out += self.bias
+        return self.out 
+    
+    def parameters(self):
+        return [self.weight] + ([] if self.bias is None else [self.bias])
+
+
+class BatchNorm1d:
+
+    def __init__(self, dim, eps = 1e-5, momentum = 0.1): #dim being number of features in 
+        self.eps = eps 
+        self.momentum = momentum
+        self.training = True
+        #parameters (trained in backdrop)
+        self.gamma = torch.ones(dim)
+        self.beta = torch.zeros(dim)
+        #buffers (trained with a running momentum update)
+        self.running_mean = torch.zeros(dim)
+        self.running_var = torch.ones(dim)
+    
+    def __call__(self, x):
+        #calculate the forward pass
+        #note x.shape = (batch_size, num_features)
+        #    Rows = different training examples
+        #    Columns = different neurons/features
+        # this is why we calculate mean of each column along the row dimensionality
+        #so we end up with xmean having 1D row of all mean values of each feature
+        if self.training:
+            xmean = x.mean(0, keepdim = True) #batch mean
+            xvar = x.var(0, keepdim = True, unbiased = True) #batch variance
+        else:
+            xmean = self.running_mean 
+            xvar = self.running_var
+        xhat = (x - xmean)/torch.sqrt(xvar + self.eps) #normalize to unit variance
+        self.out = xhat*self.gamma + self.beta
+        if self.training:
+            with torch.no_grad():
+                self.running_mean = (1 - self.momentum)*self.running_mean + self.momentum*xmean
+                self.running_var = (1 - self.momentum)*self.running_var + self.momentum*xvar
+        return self.out
+
+    def parameters(self):
+        return [self.gamma, self.beta]
+
+class Tanh:
+    def __call__(self, x):
+        self.out = torch.tanh(x)
+        return self.out
+    
+    def parameters(self):
+        return []
+
+n_embd = 10 #the dimensionality of the character embedding vectors 
+n_hidden = 100 # number of neurons in the hidden layer of the MLP
+g = torch.Generator().manual_seed(2147483647) #for reproducibiltiy 
+
+C = torch.randn((vocab_size, n_embd), generator = g)
+layers = [
+    Linear(n_embd*block_size, n_hidden), Tanh(),
+    Linear(n_hidden, n_hidden), BatchNorm1d(n_hidden), Tanh(),
+    Linear(n_hidden, n_hidden), BatchNorm1d(n_hidden), Tanh(),
+    Linear(n_hidden, n_hidden), BatchNorm1d(n_hidden), Tanh(),
+    Linear(n_hidden, n_hidden), BatchNorm1d(n_hidden), Tanh(),
+    Linear(n_hidden, vocab_size), BatchNorm1d(vocab_size),
+]
+
+with torch.no_grad():
+    #last layer: make less confident (softmax layer)
+    #layers[-1].weight *= 0.1
+    layers[-1].gamma *= 0.1 #since now the last layer is batchNorm1d, it would be .weight if the last layer was linear
+    #for other layers: apply gain 
+    for layer in layers[:-1]:
+        if isinstance(layer, Linear):
+            layer.weight *= 5/3
+            #Tanh is trying to squash the values to 0 so after several layers the std get smaller and smaller and closer to 0
+            #to off-set the squashing and keep a std close to 1 throughout the layers 
+            #we give the linear layers a gain to offset the squashing (unsure where specifically 5/3 originated from but it works well)
+            #more than the saturation becomes way too high meaning that the tanh outputs are too close to -1 and +1 
+            #this is bad because if outputs are too close to +/- 1 then the derivative is very close to almost 0
+            #then backpropagation has very little signal to tell the earlier weights how they should change
+            #we generally want the Tanh outputs to be spread more around the middle (near 0) to give a great gradient
+
+parameters = [C] + [p for layer in layers for p in layer.parameters()]
+print(sum(p.nelement() for p in parameters)) #total params
+for p in parameters:
+    p.requires_grad = True
+
+#Optimization 
+max_steps = 200000
+batch_size = 32
+lossi = []
+ud = []
+
+for i in range(max_steps):
+    ix = torch.randint(0, Xtr.shape[0], (batch_size, ), generator = g)
+    Xb, Yb = Xtr[ix], Ytr[ix]
+
+    #forward pass 
+    emb = C[Xb]
+    x = emb.view(emb.shape[0], -1) #concatenate the vectors
+    for layer in layers:
+        x = layer(x)
+    loss = F.cross_entropy(x, Yb)
+
+    #backward pass
+    for layer in layers:
+        layer.out.retain_grad() #AFTER_DEBUG: would take out retain_graph
+    for p in parameters:
+        p.grad = None
+    loss.backward()
+
+    #update
+    lr = 0.1 if i < 100000 else 0.01
+    for p in parameters:
+        p.data += -lr*p.grad
+
+     #track stats
+    if i % 10000 == 0:
+        print(f'{i:7d}/{max_steps:7d}: {loss.item(): .4f}')
+    lossi.append(loss.log10().item())
+    with torch.no_grad():
+        ud.append([(lr*p.grad.std()/p.data.std()).log10().item() for p in parameters])
+    
+    if i >= 1000:
+        break #just for debugging purposes
+
+
+# visualize histograms
+plt.figure(figsize=(20, 4)) # width and height of the plot
+legends = []
+for i, layer in enumerate(layers[:-1]): # note: exclude the output layer
+  if isinstance(layer, Tanh):
+    t = layer.out
+    print('layer %d (%10s): mean %+.2f, std %.2f, saturated: %.2f%%' % (i, layer.__class__.__name__, t.mean(), t.std(), (t.abs() > 0.97).float().mean()*100))
+    hy, hx = torch.histogram(t, density=True)
+    plt.plot(hx[:-1].detach(), hy.detach())
+    legends.append(f'layer {i} ({layer.__class__.__name__}')
+plt.legend(legends);
+plt.title('activation distribution')
+plt.show()
+
+# visualize histograms
+plt.figure(figsize=(20, 4)) # width and height of the plot
+legends = []
+for i, layer in enumerate(layers[:-1]): # note: exclude the output layer
+  if isinstance(layer, Tanh):
+    t = layer.out.grad
+    print('layer %d (%10s): mean %+f, std %e' % (i, layer.__class__.__name__, t.mean(), t.std()))
+    hy, hx = torch.histogram(t, density=True)
+    plt.plot(hx[:-1].detach(), hy.detach())
+    legends.append(f'layer {i} ({layer.__class__.__name__}')
+plt.legend(legends);
+plt.title('gradient distribution')
+plt.show()
+
+
+# visualize histograms
+plt.figure(figsize=(20, 4)) # width and height of the plot
+legends = []
+for i,p in enumerate(parameters):
+  t = p.grad
+  if p.ndim == 2:
+    print('weight %10s | mean %+f | std %e | grad:data ratio %e' % (tuple(p.shape), t.mean(), t.std(), t.std() / p.std()))
+    hy, hx = torch.histogram(t, density=True)
+    plt.plot(hx[:-1].detach(), hy.detach())
+    legends.append(f'{i} {tuple(p.shape)}')
+plt.legend(legends)
+plt.title('weights gradient distribution');
+plt.show()
+
+
+plt.figure(figsize=(20, 4))
+legends = []
+for i,p in enumerate(parameters):
+  if p.ndim == 2:
+    plt.plot([ud[j][i] for j in range(len(ud))])
+    legends.append('param %d' % i)
+plt.plot([0, len(ud)], [-3, -3], 'k') # these ratios should be ~1e-3, indicate on plot
+#if this is much higher than that, then the update is happening too fast
+#if it is below that, then the parameters are learning too slow (==> solution: increase learning rate)
+plt.legend(legends);
+plt.show()
